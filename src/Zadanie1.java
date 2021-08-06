47b4345a9138bacf1b889f495ccc8d3e7854c45b
LAPTOP-HSFC3KU9\diana <diana.kozlowska.zary@gmail.com>
2021-08-06 15:11:04 +0200

    Task1

---
+++ src/Zadanie1.java
@@ -0,0 +1,15 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int wiek = 103;
+        if (wiek<0)return;
+        if(wiek>102)return;
+        if (wiek<18){
+            System.out.println("Dziecko");}
+        else if (wiek<30){
+            System.out.println("Jest młody");}
+        else if(wiek<65){
+            System.out.println("Jest w średnim wieku");}
+        else if (wiek<=102){
+            System.out.println("Jest stary");}
+        }
+    }
