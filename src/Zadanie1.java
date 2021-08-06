3041a82c611acbfcac552f44011865712f0a290f
LAPTOP-HSFC3KU9\diana <diana.kozlowska.zary@gmail.com>
2021-08-06 16:01:54 +0200

    Aktualizacja zadania 1

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -1,15 +1,16 @@
 public class Zadanie1 {
     public static void main(String[] args) {
-        int wiek = 103;
-        if (wiek<0)return;
-        if(wiek>102)return;
-        if (wiek<18){
-            System.out.println("Dziecko");}
-        else if (wiek<30){
-            System.out.println("Jest młody");}
-        else if(wiek<65){
-            System.out.println("Jest w średnim wieku");}
-        else if (wiek<=102){
-            System.out.println("Jest stary");}
+        int wiek = 102;
+        if (wiek < 0) return;
+        if (wiek > 102) return;
+        if (wiek < 18) {
+            System.out.println("Dziecko");
+        } else if (wiek < 30) {
+            System.out.println("Jest młody");
+        } else if (wiek < 65) {
+            System.out.println("Jest w średnim wieku");
+        } else {
+            System.out.println("Jest stary");
         }
     }
+}
