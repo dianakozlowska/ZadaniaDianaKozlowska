0c94c7869c1e9d0609082690c7e09c16e096243c
LAPTOP-HSFC3KU9\diana <diana.kozlowska.zary@gmail.com>
2021-09-01 17:03:07 +0200

    Zadanie 3- Diana Kozłowska - poprawione

--- src/Zadanie2.java
+++ src/Zadanie2.java
@@ -1,25 +1,26 @@
 public class Zadanie2 {
     public static void main(String[] args) {
-        for (int y = 0; y < 7; y++) {
-            int rowNo = y+1;
+        int row = 9;
+        for (int y = 0; y < row; y++) {
+            int rowNo = y + 1;
             System.out.print(rowNo + ": ");
-            for (int i = 7; i > y; i--) {
+            for (int i = row; i > y; i--) {
                 System.out.print(" ");
             }
-            int g = 2*y+1;
+            int g = 2 * y + 1;
             for (int stars = 0; stars < g; stars++) {
                 System.out.print("*");
             }
 
             System.out.println();
         }
-        for (int y = 7; y >= 0; y--) {
-            int rowNo = y+1;
+        for (int y = row; y >= 0; y--) {
+            int rowNo = y + 1;
             System.out.print(rowNo + ": ");
-            for (int i = 7; i > y; i--) {
+            for (int i = row; i > y; i--) {
                 System.out.print(" ");
             }
-            int g = 2*y+1;
+            int g = 2 * y + 1;
             for (int stars = 0; stars < g; stars++) {
                 System.out.print("*");
             }
