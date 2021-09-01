d5c200f0816f82e8c9fc071b939648df7cbae288
LAPTOP-HSFC3KU9\diana <diana.kozlowska.zary@gmail.com>
2021-09-01 16:59:05 +0200

    Zadanie 3- Diana Kozłowska - poprawione

--- src/Zadanie3.java
+++ src/Zadanie3.java
@@ -1,22 +1,27 @@
 public class Zadanie3 {
     public static void main(String[] args) {
         int randomNum = 0;
-        int max = 0;
-        int min = 0;
         int[] tab = new int[8];
         for (int i = 0; i < tab.length; i++) {
             randomNum = (int) (Math.random() * 10);
             tab[i] = randomNum;
             System.out.print(tab[i] + ", ");
+        }
+        int max = 0;
+        int min = 0;
+        for (int i = 0; i < tab.length; i++) {
+            randomNum = (int) (Math.random() * 10);
+            tab[i] = randomNum;
             tab[1] = max;
             if (tab[i] > max) max = tab[i];
             tab[1] = min;
             if (tab[i] < min) min = tab[i];
-
         }
-        System.out.println();
-        System.out.println("max=" + max);
-        System.out.println("min=" + min);
-        System.out.println("koniec");
+        {
+            System.out.println();
+            System.out.println("max=" + max);
+            System.out.println("min=" + min);
+            System.out.println("koniec");
+        }
     }
 }
